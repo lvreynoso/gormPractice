0cf75c65ff7357ef06edc106e05bbc8434d6e0e4
Lucia Reynoso <lvreynoso@protonmail.com>
2019-02-11 02:03:33 -0800

    added create and read routes, having trouble with update

--- server.go
+++ server.go
@@ -3,16 +3,12 @@
 package main
 
 import (
-"time"
-"errors"
 "fmt"
 "encoding/json"
 "io/ioutil"
 "log"
 "net/http"
 "github.com/labstack/echo"
-"strings"
-"math/rand"
 "github.com/jinzhu/gorm"
 _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -22,31 +18,20 @@ _ "github.com/jinzhu/gorm/dialects/sqlite"
 // [DONE] call another API with similar content to original ChuckJoke
 
 // Takes in Chuck Norris API; used in Taco struct
-type ChuckJoke struct {
-    ID int `json:"id"`
-    Joke string `json:"joke"`
-    Categories []string `json:"categories"`
+type Norris struct {
+    gorm.Model
+    JokeID int `json:"id" form:"jokeid" query:"jokeid"`
+    Joke string `json:"joke" form:"joke" query:"joke"`
+    // Categories []string `json:"categories"`
 }
 
 // Takes in ChuckJoke struct; used in texasRanger
 type Taco struct {
     Type string `json:"type"`
-    Value ChuckJoke `json:"value"`
+    Value Norris `json:"value"`
 }
 
-// Takes in tronalddump API; used in TrumpDump struct
-type Dumps struct {
-    Value string `json:"value"`
-}
-
-// Takes in TrumpDump struct; used in func newYorkBarFly
-type TrumpQuotes struct {
-    Embedded struct {
-        Quotes []Dumps `json:"quotes"`
-    } `json:"_embedded"`
-}
-
-func texasRanger() string {
+func texasRanger() Taco {
     // takes in Taco struct and returns a Chuck Norris jok as a string
     response, err := http.Get("https://api.icndb.com/jokes/random")
     if err != nil {
@@ -65,110 +50,125 @@ func texasRanger() string {
         log.Fatalln(jsonErr)
     }
 
-    return taco.Value.Joke
-}
-
-func newYorkBarFly(vomit []string) (_ string, err error) {
-
-    fmt.Println("WELCOME TO NEW YORK")
-
-    chalupa := TrumpQuotes{}
-
-    for index, word := range vomit {
-
-        fmt.Printf("FOR THE %d TIME\n", index + 1)
-
-        if len(word) < 4 {
-            fmt.Println("Wow, this word sucks! It has 3 or fewer characters in it!")
-            continue
-        }
-
-        fmt.Println("Our word is longer than 3 characters!")
-
-        response, responseErr := http.Get("https://api.tronalddump.io/search/quote?query=" + word)
-        if responseErr != nil {
-            fmt.Println("Error getting a response")
-            log.Fatalln(err)
-            continue
-        }
-
-        fmt.Println("We got a response!")
-
-        body, bodyErr := ioutil.ReadAll(response.Body)
-        if bodyErr != nil {
-            fmt.Println("Error reading the response body")
-            log.Fatalln(err)
-            continue
-        }
-
-        fmt.Println("We read the body!")
-
-        rawChalupa := TrumpQuotes{}
-
-        jsonErr := json.Unmarshal(body, &rawChalupa)
-        if jsonErr != nil {
-            fmt.Println("Oh shit! JSON couldn't be unmarsheled! Trying again!")
-            continue
-        } else if len(rawChalupa.Embedded.Quotes) < 1 {
-            fmt.Println("No quotes! Try again!")
-            continue
-        } else {
-            fmt.Println("We unmarsheled that JSON and got some quotes!")
-            chalupa = rawChalupa
-            break
-        }
-    }
-
-    fmt.Println("Chalupa:")
-    fmt.Println(chalupa.Embedded.Quotes)
-
-    trumpQuote := ""
-
-    if len(chalupa.Embedded.Quotes) > 0 {
-        err = nil
-        trumpQuote = chalupa.Embedded.Quotes[rand.Intn(len(chalupa.Embedded.Quotes))].Value
-    } else {
-        fmt.Println("Wow this quote sucks! Tronald Dump hasn't said anything about any of these words!")
-        err = errors.New("Chuck Norris? Never heard of him.")
-    }
-
-    return trumpQuote, err
+    return taco
 }
 
 func main() {
     server := echo.New()
 
-    rand.Seed(time.Now().Unix())
+    db, err := gorm.Open("sqlite3", "practice.db")
+      if err != nil {
+        panic("failed to connect database")
+      }
+    defer db.Close()
+
+    db.AutoMigrate(&Norris{})
 
     server.GET("/", func(context echo.Context) error {
-        tacoFilling := texasRanger()
-        groundBeef := strings.Split(tacoFilling, " ")
-        meatMap := make(map[string]int)
-        for _, beef := range groundBeef {
-            if _, ok := meatMap[beef]; ok {
-                meatMap[beef] += 1
-            } else {
-                meatMap[beef] = 1
-            }
-        }
-
-        hotWord := make([]string, 0)
-        for key, value := range meatMap {
-            if value == 1 {
-                hotWord = append(hotWord, key)
-            }
-        }
-        fmt.Println(`Chuck's words are:`)
-        fmt.Println(hotWord)
-        upchuck, err := newYorkBarFly(hotWord)
-
+        rows, err := db.Model(&Norris{}).Rows()
+        defer rows.Close()
         if err != nil {
-            upchuck = err.Error()
+            fmt.Println(err)
+            return context.HTML(http.StatusInternalServerError, "Womp womp")
         }
 
-        return context.HTML(http.StatusOK, "<p><em>" + tacoFilling + "</em>" + `<br><em>- Faith Chikwekwe</em></p><br><p><em>` + upchuck + `</em><br><em>- Lucia Reynoso</em></p>`)
+        jokeHTML := "<p>"
 
-    })
+        for rows.Next() {
+            var norris Norris
+            db.ScanRows(rows, &norris)
+            jokeHTML += norris.Joke + "<br>"
+        }
+
+        jokeHTML += "</p>"
+
+        return context.HTML(http.StatusOK, "<h1>Success!</h1><br><h4>Current Jokes:</h4>" + jokeHTML)
+        })
+
+    server.GET("/upload", func(context echo.Context) error {
+            uploadForm := `
+            <h3>Chuck Norris joke uploader for Golang</h3>
+            <br>
+            <form action="/upload" method="post" enctype="multipart/form-data">
+                <div>
+                    <label for="jokeid">Enter a Joke ID:</label><br>
+                    <input type="text" id="jokeid" name="jokeid" value="">
+                </div>
+                <br>
+                <div>
+                    <label for="joke">Enter a Chuck Norris Joke:</label><br>
+                    <input type="text" id="Joke" name="joke" value="">
+                </div>
+                <br>
+                <div>
+                    <button type="submit" name="button">Upload</button>
+                </div>
+            </form>`
+            return context.HTML(http.StatusOK, uploadForm)
+        })
+
+    server.POST("/upload", func(context echo.Context) error {
+        norris := new(Norris)
+        if err = context.Bind(norris); err != nil {
+            fmt.Println("Womp womped on the bind bind")
+            return context.HTML(http.StatusInternalServerError, "Womp womp")
+        }
+
+        // put the joke in the table
+        db.Create(&Norris{JokeID: norris.JokeID, Joke: norris.Joke})
+
+        return context.JSON(http.StatusOK, norris)
+        })
+
+    server.GET("/update", func(context echo.Context) error {
+            uploadForm := `
+            <h3>Chuck Norris joke uPdAtEr for Golang</h3>
+            <br>
+            <form action="/update" method="PUT" enctype="multipart/form-data">
+                <div>
+                    <label for="jokeid">Enter the Joke ID:</label><br>
+                    <input type="text" id="jokeid" name="jokeid" value="">
+                </div>
+                <br>
+                <div>
+                    <label for="joke">Enter the new Chuck Norris Joke:</label><br>
+                    <input type="text" id="Joke" name="joke" value="">
+                </div>
+                <br>
+                <div>
+                    <button type="submit" name="button">Upload</button>
+                </div>
+            </form>`
+            return context.HTML(http.StatusOK, uploadForm)
+        })
+
+    server.PUT("/update", func(context echo.Context) error {
+        updated := new(Norris)
+        if err = context.Bind(updated); err != nil {
+            fmt.Println("Womp womped on the bind bind")
+            return context.HTML(http.StatusInternalServerError, "Womp womp")
+        }
+
+        var norris Norris
+
+        db.Where(&Norris{JokeID: updated.JokeID}).First(&norris)
+
+        norris.Joke = updated.Joke
+
+        db.Save(&norris)
+
+        return context.JSON(http.StatusOK, norris)
+        })
+
+    server.GET("/populate", func(context echo.Context) error {
+        taco := texasRanger()
+        tacoFilling := taco.Value
+
+        // create a new joke in SQL
+        db.Create(&Norris{JokeID: tacoFilling.JokeID, Joke: tacoFilling.Joke})
+
+        return context.HTML(http.StatusOK, "<h1>SQLized!</h1><p><em>" + tacoFilling.Joke + "</em>" + `<br><em>- Faith Chikwekwe</em></p>`)
+        })
 
     server.Logger.Fatal(server.Start(":9001"))
 }
